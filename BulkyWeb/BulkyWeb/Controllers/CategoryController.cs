44ea9744d83948e135fc4714d24fa46c33604b3c
Ammarhussain50 <Hussainammar205@gmail.com>
2025-11-07 23:11:32 +0500

    Category added

---
+++ BulkyWeb/BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
