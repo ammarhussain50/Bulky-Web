b76230c2acbaf78ecad46f29c61478bcc5ca4eb5
Ammarhussain50 <Hussainammar205@gmail.com>
2025-11-13 02:11:30 +0500

    added product CRUD in controller and view page

--- BulkyWeb/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+012f436c3b4c153588b0771601ad51d01e905a6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+310cca29b050b433cc64e9545b686392250cca78")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
