2df86a8ab7ef42dbb79ca62870c7fdb3b7f7fa8f
Ammarhussain50 <Hussainammar205@gmail.com>
2025-12-25 00:57:35 +0500

    Sweet Alert Added on Delete

--- BulkyWeb/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f2359f4216eab90ab5758e2aaa48b50cb72a647")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c770b3d198a9d643b80475225c0c508b5378c72c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
