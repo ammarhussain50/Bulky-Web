33d6b6712b2bad74ae9d6dccbc13e7408c1d6ad1
Ammarhussain50 <Hussainammar205@gmail.com>
2025-12-25 22:57:32 +0500

    displaying products in home page

--- BulkyWeb/BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-using System.Diagnostics;
+using Bulky.DataAccess.Repository;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace BulkyWeb.Areas.Customer.Controllers
 {
@@ -7,16 +10,19 @@ namespace BulkyWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger , IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
         }
 
         public IActionResult Privacy()
--- BulkyWeb/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c770b3d198a9d643b80475225c0c508b5378c72c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2df86a8ab7ef42dbb79ca62870c7fdb3b7f7fa8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
