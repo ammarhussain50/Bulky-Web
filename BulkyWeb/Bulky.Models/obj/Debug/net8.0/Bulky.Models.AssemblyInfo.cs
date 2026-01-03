ee78cc6155060d8bb7ad9483d01ae2019371613d
Ammarhussain50 <Hussainammar205@gmail.com>
2026-01-04 01:30:04 +0500

    Company Table created and all CRUD func completed

--- BulkyWeb/Bulky.Models/obj/Debug/net8.0/Bulky.Models.AssemblyInfo.cs
+++ BulkyWeb/Bulky.Models/obj/Debug/net8.0/Bulky.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f39fd43353190ef23a459d51156ff3e1ad526e91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72c37f24d844ee23c29697cc8ce0926518dfa894")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BulkyWeb/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f39fd43353190ef23a459d51156ff3e1ad526e91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72c37f24d844ee23c29697cc8ce0926518dfa894")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
